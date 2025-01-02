7658dd47ab3bd4e448bfa31ab566a4833376d3b8
Hamza Rafeeq <hamzarafeeq08@gmail.com>
2025-01-02 05:33:55 -0800

    proxy server updated

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require('express')
+const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
-const app = express()
-app.use(cors())
 
+const app = express();
+app.use(cors());
+
+// Proxy setup
 const apiProxy = createProxyMiddleware({
-    target: 'https://maps.googleapis.com/maps/api/place/autocomplete/json',
-    changeOrigin: true,
-  });
-  app.use('/api', apiProxy);
-  app.listen(8080, ()=>{
-    console.log('server is running');
-  })
-  
+  target: 'https://maps.googleapis.com',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '', // Remove the '/api' prefix before forwarding to the target
+  }
+});
+
+app.use('/api', apiProxy);
+
+app.listen(8080, () => {
+  console.log('Server is running');
+});
