221dfd4a71c5e1986934236912ec7dc69ba22f6e
Hamza Rafeeq <hamzarafeeq08@gmail.com>
2025-01-02 23:00:44 -0800

    code updated

--- index.js
+++ index.js
@@ -8,17 +8,13 @@ app.use(cors());
 
 // Proxy setup
 const apiProxy = createProxyMiddleware({
-  target: (req) => {
-    // Extract input from the request parameters
-    const { input } = req.params;
-    // Dynamically construct the target URL with the input value
-    return `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${process.env.GOOGLE_API_KEY}&input=pak`;
-  },
+  target: `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${process.env.GOOGLE_API_KEY}&input=fais`,
+  
   changeOrigin: true,
   // You don't need to use pathRewrite in this case
 });
 
-app.use('/api/:input', apiProxy);
+app.use('/api', apiProxy);
 
 app.listen(8080, () => {
   console.log('Server is running');
